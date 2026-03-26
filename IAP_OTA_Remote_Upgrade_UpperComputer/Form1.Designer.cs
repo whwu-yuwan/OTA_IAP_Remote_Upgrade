6831396e8cc00e00871ec621fe39020379874004
Yuwan <1445811372@qq.com>
2026-03-26 23:33:27 +0800

    上位机后端开发与前端优化

--- IAP_OTA_Remote_Upgrade_UpperComputer/Form1.Designer.cs
+++ IAP_OTA_Remote_Upgrade_UpperComputer/Form1.Designer.cs
@@ -50,6 +50,11 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button6 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.通讯设置区.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -57,16 +62,15 @@
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // 通讯设置区
             // 
             this.通讯设置区.Controls.Add(this.tabControl1);
-            this.通讯设置区.Location = new System.Drawing.Point(16, 15);
-            this.通讯设置区.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.通讯设置区.Location = new System.Drawing.Point(12, 12);
             this.通讯设置区.Name = "通讯设置区";
-            this.通讯设置区.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.通讯设置区.Size = new System.Drawing.Size(211, 238);
+            this.通讯设置区.Size = new System.Drawing.Size(159, 190);
             this.通讯设置区.TabIndex = 0;
             this.通讯设置区.TabStop = false;
             this.通讯设置区.Text = "通讯设置区";
@@ -76,11 +80,10 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(8, 25);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Location = new System.Drawing.Point(6, 20);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(191, 201);
+            this.tabControl1.Size = new System.Drawing.Size(143, 161);
             this.tabControl1.TabIndex = 6;
             // 
             // tabPage1
@@ -88,11 +91,10 @@
             this.tabPage1.Controls.Add(this.comboBox2);
             this.tabPage1.Controls.Add(this.comboBox1);
             this.tabPage1.Controls.Add(this.button2);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage1.Size = new System.Drawing.Size(183, 172);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(135, 135);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "串口控件";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -100,29 +102,26 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(8, 19);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Location = new System.Drawing.Point(6, 15);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(160, 23);
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
             this.comboBox2.TabIndex = 5;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(7, 70);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Location = new System.Drawing.Point(5, 56);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 23);
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(7, 124);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(5, 99);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 29);
+            this.button2.Size = new System.Drawing.Size(121, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "打开/关闭";
             this.button2.UseVisualStyleBackColor = true;
@@ -133,62 +132,57 @@
             this.tabPage2.Controls.Add(this.textBox1);
             this.tabPage2.Controls.Add(this.button1);
             this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage2.Size = new System.Drawing.Size(183, 172);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(135, 135);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "网络控件";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(8, 24);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(159, 25);
+            this.textBox1.Size = new System.Drawing.Size(120, 21);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 126);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(6, 101);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 29);
+            this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "连接/断开";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(8, 79);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(6, 63);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(159, 25);
+            this.textBox2.Size = new System.Drawing.Size(120, 21);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Controls.Add(this.button3);
             this.groupBox3.Controls.Add(this.textBox3);
             this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Location = new System.Drawing.Point(16, 261);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Location = new System.Drawing.Point(12, 209);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox3.Size = new System.Drawing.Size(211, 238);
+            this.groupBox3.Size = new System.Drawing.Size(159, 318);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "固件加载区";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(4, 79);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(3, 32);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(187, 38);
+            this.button3.Size = new System.Drawing.Size(140, 30);
             this.button3.TabIndex = 2;
             this.button3.Text = "浏览";
             this.button3.UseVisualStyleBackColor = true;
@@ -196,61 +190,57 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(4, 26);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Location = new System.Drawing.Point(3, 79);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(187, 25);
+            this.textBox3.Size = new System.Drawing.Size(140, 21);
             this.textBox3.TabIndex = 1;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1, 144);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(8, 127);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.progressBar1);
             this.groupBox2.Controls.Add(this.button5);
             this.groupBox2.Controls.Add(this.button4);
-            this.groupBox2.Location = new System.Drawing.Point(13, 508);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Location = new System.Drawing.Point(201, 13);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(211, 238);
+            this.groupBox2.Size = new System.Drawing.Size(161, 190);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "升级控制区";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(7, 182);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Location = new System.Drawing.Point(5, 146);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(187, 26);
+            this.progressBar1.Size = new System.Drawing.Size(140, 21);
             this.progressBar1.TabIndex = 6;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(7, 106);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Location = new System.Drawing.Point(5, 85);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(187, 36);
+            this.button5.Size = new System.Drawing.Size(140, 29);
             this.button5.TabIndex = 5;
             this.button5.Text = "开始升级";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(7, 39);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(5, 31);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(187, 39);
+            this.button4.Size = new System.Drawing.Size(140, 31);
             this.button4.TabIndex = 4;
             this.button4.Text = "握手/进入boot";
             this.button4.UseVisualStyleBackColor = true;
@@ -265,45 +255,92 @@
             // 
             this.groupBox1.Controls.Add(this.button6);
             this.groupBox1.Controls.Add(this.richTextBox1);
-            this.groupBox1.Location = new System.Drawing.Point(235, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(420, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(643, 730);
+            this.groupBox1.Size = new System.Drawing.Size(482, 584);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "日志显示区";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(7, 692);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Location = new System.Drawing.Point(6, 555);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(161, 29);
+            this.button6.Size = new System.Drawing.Size(121, 23);
             this.button6.TabIndex = 6;
             this.button6.Text = "清空日志";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(0, 25);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Location = new System.Drawing.Point(5, 19);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(628, 642);
+            this.richTextBox1.Size = new System.Drawing.Size(472, 531);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox3);
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Location = new System.Drawing.Point(201, 209);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(161, 318);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "指令发送区";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(5, 91);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(140, 30);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "发送";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_3);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(6, 42);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(139, 20);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(891, 754);
+            this.ClientSize = new System.Drawing.Size(914, 603);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.通讯设置区);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -316,6 +353,7 @@
             this.groupBox3.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -344,6 +382,12 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- IAP_OTA_Remote_Upgrade_UpperComputer/Form1.cs
+++ IAP_OTA_Remote_Upgrade_UpperComputer/Form1.cs
@@ -13,8 +13,22 @@ namespace IAP_OTA_Remote_Upgrade_UpperComputer
 {
     public partial class Form1 : Form
     {
-        public const UInt32 HEADER = 0xAA55;
+        public const UInt16 HEADER = 0xAA55;
 
+        // 定义协议命令码
+        public enum OtaCommand : ushort
+        {
+            CMD_PING = 0x0100,
+            CMD_RESET = 0x0101,
+            CMD_GET_VERSION = 0x0102,
+            CMD_PARAM_READ = 0x0500,
+            CMD_PARAM_WRITE = 0x0501,
+            CMD_UPDATE_START = 0x0300,
+            CMD_UPDATE_DATA = 0x0301,
+            CMD_UPDATE_END = 0x0302,
+            CMD_ACK = 0xFF00,
+            CMD_NACK = 0xFF01
+        }
 
         SerialPort sp1 = new SerialPort();
         public Form1()
@@ -34,7 +48,91 @@ namespace IAP_OTA_Remote_Upgrade_UpperComputer
 
         private void button4_Click(object sender, EventArgs e)
         {
-            sp1.Write ("");
+            // 点击"握手/进入boot"时，发送链路测试/心跳包
+            SendProtocolData(OtaCommand.CMD_PING);
+        }
+
+        /// <summary>
+        /// 基于协议格式打包数据，并发送至串口
+        /// </summary>
+        private void SendProtocolData(OtaCommand command, byte[] data = null)
+        {
+            if (!sp1.IsOpen)
+            {
+                MessageBox.Show("请先打开串口！");
+                return;
+            }
+
+            try
+            {
+                List<byte> frame = new List<byte>();
+
+                // 1. Header (2 bytes) - 0xAA55 (C# BitConverter 会转为小端发送: 0x55, 0xAA)
+                frame.AddRange(BitConverter.GetBytes((ushort)HEADER));
+
+                // 2. Command (2 bytes)
+                frame.AddRange(BitConverter.GetBytes((ushort)command));
+
+                // 3. Length (2 bytes)
+                ushort dataLen = (ushort)(data == null ? 0 : data.Length);
+                frame.AddRange(BitConverter.GetBytes(dataLen));
+
+                // 4. Data (N bytes)
+                if (dataLen > 0)
+                {
+                    frame.AddRange(data);
+                }
+
+                // 5. Reserved (4 bytes)
+                frame.AddRange(new byte[] { 0x00, 0x00, 0x00, 0x00 });
+
+                // 6. CRC16 (2 bytes)
+                ushort crc = CalculateCRC16(frame.ToArray());
+                frame.AddRange(BitConverter.GetBytes(crc));
+
+                byte[] packet = frame.ToArray();
+
+                // 发送数据
+                sp1.Write(packet, 0, packet.Length);
+
+                // 日志打印 TX 记录 (蓝色)
+                string hexData = BitConverter.ToString(packet).Replace("-", " ") + " ";
+                this.Invoke(new Action(() =>
+                {
+                    richTextBox1.SelectionColor = Color.Blue;
+                    richTextBox1.AppendText("[TX] " + hexData + "\n");
+                    richTextBox1.ScrollToCaret();
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("发送数据失败: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 计算 CRC16-MODBUS 校验码
+        /// </summary>
+        private ushort CalculateCRC16(byte[] data)
+        {
+            ushort crc = 0xFFFF;
+            for (int i = 0; i < data.Length; i++)
+            {
+                crc ^= data[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x0001) != 0)
+                    {
+                        crc >>= 1;
+                        crc ^= 0xA001;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+            return crc;
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
@@ -49,7 +147,25 @@ namespace IAP_OTA_Remote_Upgrade_UpperComputer
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // 设置文件选择对话框
+            openFileDialog1.Title = "请选择固件升级包";
+            openFileDialog1.Filter = "Bin文件 (*.bin)|*.bin|所有文件 (*.*)|*.*";
+            openFileDialog1.FileName = ""; // 清空默认文本
 
+            // 如果用户点击了“确定”
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // 获取文件路径并显示在文本框
+                string filePath = openFileDialog1.FileName;
+                textBox3.Text = filePath;
+
+                // 获取文件大小
+                System.IO.FileInfo fileInfo = new System.IO.FileInfo(filePath);
+                long fileSizeBytes = fileInfo.Length;
+
+                // 在 label 处打印文件大小，格式化为字节和KB
+                label1.Text = $"文件大小：{fileSizeBytes} 字节 ({fileSizeBytes / 1024.0:F2} KB)";
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -61,12 +177,12 @@ namespace IAP_OTA_Remote_Upgrade_UpperComputer
         {
             try
             {
-                if (sp1.IsOpen)
+                if (!sp1.IsOpen)
                 {
                     sp1.PortName = comboBox2.Text;
                     sp1.BaudRate = int.Parse(comboBox1.Text);
                     sp1.DataBits = 8;
-                    sp1.StopBits = 0;
+                    sp1.StopBits = StopBits.One;
                     sp1.Parity = Parity.None;
 
                     sp1.Open();
@@ -105,10 +221,103 @@ namespace IAP_OTA_Remote_Upgrade_UpperComputer
                "9600"
             });
 
-            // 默认选中 115200
-            comboBox1.SelectedIndex = 4;
+            // 默认选中 115200 (注意：115200在第0位)
+            comboBox1.SelectedIndex = 0;
 
             comboBox2.Items.AddRange(SerialPort.GetPortNames());
+
+            // 绑定非升级指令到 comboBox3
+            comboBox3.Items.AddRange(new string[] 
+            {
+                "链路测试/心跳 (CMD_PING)",
+                "请求设备软复位 (CMD_RESET)",
+                "获取设备版本信息 (CMD_GET_VERSION)",
+                "读取设备Flash参数区摘要 (CMD_PARAM_READ)"
+            });
+            comboBox3.SelectedIndex = 0; // 默认选中第一个
+
+            // 注册串口接收事件
+            sp1.DataReceived += Sp1_DataReceived;
+        }
+
+        // 串口接收事件处理函数
+        private void Sp1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // 读取缓冲区所有字节
+            int bytesToRead = sp1.BytesToRead;
+            byte[] buffer = new byte[bytesToRead];
+            sp1.Read(buffer, 0, bytesToRead);
+
+            // 转为十六进制字符串 (因为是OTA协议，以HEX展示更方便调试)
+            string hexData = BitConverter.ToString(buffer).Replace("-", " ") + " ";
+
+            // 使用 Invoke 跨线程安全更新 UI
+            this.Invoke(new Action(() =>
+            {
+                richTextBox1.SelectionColor = Color.Green; // 接收数据用绿色显示
+                richTextBox1.AppendText("[RX] " + hexData + "\n");
+                richTextBox1.ScrollToCaret(); // 自动滚动
+            }));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click_3(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedIndex == 0)
+            {
+                SendProtocolData(OtaCommand.CMD_PING);
+            }
+            else if (comboBox3.SelectedIndex == 1)
+            {
+                SendProtocolData(OtaCommand.CMD_RESET);
+            }
+            else if (comboBox3.SelectedIndex == 2)
+            {
+                SendProtocolData(OtaCommand.CMD_GET_VERSION);
+            }
+            else if (comboBox3.SelectedIndex == 3)
+            {
+                SendProtocolData(OtaCommand.CMD_PARAM_READ);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
